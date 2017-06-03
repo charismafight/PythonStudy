9783e0379d9f1e43dea4873032d2d231b4a37a33
charismafight <charismafight@hotmail.com>
2017-06-04 01:34:54 +0800

    PicTrans

---
+++ PicTransWords/Trans.py
@@ -0,0 +1,40 @@
+# image package has func like 'open' so we rename it
+import PIL.Image as Image
+import argparse
+import os
+import sys
+
+# symbol list,choose different to show pic
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+l = len(ascii_char)
+# count how many grays a symbol to represent
+# because we have 70 symbols in ascii_char use 256/l=3.xxxx
+# then a word can represent 3.xxxx gray or 1-3.xxx use '$' rest are the same manner
+unit = 256 / l
+
+
+def get_char(r, g, b, alfpha=256):
+    if alfpha == 0:
+        return ' '
+
+    # formula of gray
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+    # gray //unit get the symbols' index in the list
+    return ascii_char[int(gray // unit)]
+
+
+if __name__ == '__main__':
+    p = sys.path[0]
+    # open and reseize img then read all pixels use a symbol instead
+    pic = Image.open(p + '/PicTransWords/Pics/ascii_dora.png')
+    pic = pic.resize((80, 80), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(80):
+        for j in range(80):
+            txt += get_char(*pic.getpixel((j, i)))
+
+        txt += '\n'
+
+    print(txt)
