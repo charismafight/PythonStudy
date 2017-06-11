8c3cc52f074d639205127c16a8b66d1e9089ad54
lee_comp <charismafight@hotmail.com>
2017-06-11 17:09:31 +0800

    add part of argparse

--- Argparse/ArgParse.py
+++ Argparse/ArgParse.py
@@ -1,6 +1,7 @@
 import argparse
 
 p = argparse.ArgumentParser(description='this is my description of py file,consider this file as a command')
-p.add_argument('integers',type=int,help='this is a gorgeous language')
-myargs = p.parse_args()
-print(myargs.integers**2)
+p.add_argument('integers', type=int, help='this is a gorgeous language', nargs=2)
+p.add_argument('--foo', action='store_const', const=42)
+myargs = p.parse_args('--foo'.split())
+print(myargs.integers[0] * myargs.integers[1])
